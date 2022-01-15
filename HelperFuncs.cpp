4c278ac2379e3f1ac4380822d6db04c92db3938e
prashantpandey <prashant.prashn@gmail.com>
2022-01-15 01:16:36 -0500

    Moving the print vector functions to the HelperFuncs file so that everything can use it.

---
+++ HelperFuncs.cpp
@@ -0,0 +1,60 @@
+#include "HelperFuncs.h"
+#include <iostream>
+
+void print_binary_uint64(uint64_t x, bool newline=false, int divider=64) {
+    for(int i=0; i < 64; i++) {
+        if(i>0 && i%divider == 0) cout << ' ';
+        cout << (x%2);
+        x/=2;
+    }
+    if(newline) cout << endl;
+}
+
+void print_hex_uint64(uint64_t x, bool newline=false, int divider=64) {
+    char digit_to_char[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    for(int i=0; i < 16; i++) {
+        if(i>0 && i%divider == 0) cout << ' ';
+        cout << digit_to_char[x%16];
+        x/=16;
+    }
+    if(newline) cout << endl;
+}
+
+void print_vec(__m512i X, bool binary, int divider=64) { //either prints binary or hex in little endian fashion because of weirdness
+    if(binary) cout << "0b ";
+    else cout << "0x ";
+    for(int i=0; i < 8; i++) {
+        if(binary)
+            print_binary_uint64(X[i], false, divider);
+        else 
+            cout << X[i];
+        cout << ' ';
+    }
+    cout << endl;
+}
+
+void print_vec(__m256i X, bool binary, int divider=64) { //either prints binary or hex in little endian fashion because of weirdness
+    if(binary) cout << "0b ";
+    else cout << "0x ";
+    for(int i=0; i < 4; i++) {
+        if(binary)
+            print_binary_uint64(X[i], false, divider);
+        else 
+            cout << X[i];
+        cout << ' ';
+    }
+    cout << endl;
+}
+
+void print_vec(__m128i X, bool binary, int divider=64) { //either prints binary or hex in little endian fashion because of weirdness
+    if(binary) cout << "0b ";
+    else cout << "0x ";
+    for(int i=0; i < 2; i++) {
+        if(binary)
+            print_binary_uint64(X[i], false, divider);
+        else 
+            cout << X[i];
+        cout << ' ';
+    }
+    cout << endl;
+}
---
+++ HelperFuncs.h
@@ -0,0 +1,15 @@
+#ifndef HELPER_FUNCS_INCLUDED
+#define HELPER_FUNCS_INCLUDED
+
+#include <immintrin.h>
+#include <cstdint>
+
+using namespace std; //seriously don't do this lol
+
+void print_binary_uint64(uint64_t x, bool newline=false, int divider=64);
+void print_hex_uint64(uint64_t x, bool newline=false, int divider=64);
+void print_vec(__m512i X, bool binary, int divider=64);
+void print_vec(__m256i X, bool binary, int divider=64);
+void print_vec(__m128i X, bool binary, int divider=64);
+
+#endif 
